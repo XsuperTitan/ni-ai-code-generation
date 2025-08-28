8f8a9ab168ccf41a53c129f2488e182953c3f12c
RTX5090 <598933756@qq.com>
2025-08-28 21:18:24 +0800

    Invoke MyBatisCodeGenerator to generate code.

--- src/main/java/com/nini/niaicodeking/NiAiCodeKingApplication.java
+++ src/main/java/com/nini/niaicodeking/NiAiCodeKingApplication.java
@@ -1,12 +1,23 @@
 package com.nini.niaicodeking;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Spring Boot应用程序的主启动类
+ * 使用@SpringBootApplication注解标记这是一个Spring Boot应用
+ */
 @SpringBootApplication
+@MapperScan("com.nini.niaicodeking.mapper")
 public class NiAiCodeKingApplication {
 
+    /**
+     * 程序的入口方法
+     * @param args 命令行参数
+     */
     public static void main(String[] args) {
+        // 使用SpringApplication.run()方法启动Spring Boot应用
         SpringApplication.run(NiAiCodeKingApplication.class, args);
     }
 
