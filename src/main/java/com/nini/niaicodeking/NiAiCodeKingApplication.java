8dc8a3be9569ccd9d076917841b2fa588e4ddeef
RTX5090 <598933756@qq.com>
2025-08-28 11:19:24 +0800

    Initialize the project with a dummy file to avoid git errors

---
+++ src/main/java/com/nini/niaicodeking/NiAiCodeKingApplication.java
@@ -0,0 +1,13 @@
+package com.nini.niaicodeking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NiAiCodeKingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NiAiCodeKingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nini/niaicodeking/NiAiCodeKingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nini.niaicodeking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NiAiCodeKingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
