ca5aba538ef73877e88af896fc34f182ae06a84d
RTX5090 <598933756@qq.com>
2026-04-22 23:35:41 +0800

    Invoke MyBatisCodeGenerator to generate code.

--- src/main/java/com/nini/niaicodeking/model/entity/User.java
+++ src/main/java/com/nini/niaicodeking/model/entity/User.java
@@ -9,6 +9,7 @@ import java.time.LocalDateTime;
 
 import java.io.Serial;
 
+import com.mybatisflex.core.keygen.KeyGenerators;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -26,13 +27,14 @@ import lombok.NoArgsConstructor;
 @Table("user")
 public class User implements Serializable {
 
+
     @Serial
     private static final long serialVersionUID = 1L;
 
     /**
      * id
      */
-    @Id(keyType = KeyType.Auto)
+    @Id(keyType = KeyType.Generator, value = KeyGenerators.snowFlakeId)
     private Long id;
 
     /**
--- src/main/java/com/nini/niaicodeking/service/UserService.java
+++ src/main/java/com/nini/niaicodeking/service/UserService.java
@@ -1,7 +1,14 @@
 package com.nini.niaicodeking.service;
 
+import com.mybatisflex.core.query.QueryWrapper;
 import com.mybatisflex.core.service.IService;
+import com.nini.niaicodeking.model.dto.user.UserQueryRequest;
 import com.nini.niaicodeking.model.entity.User;
+import com.nini.niaicodeking.model.vo.LoginUserVO;
+import com.nini.niaicodeking.model.vo.UserVO;
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.util.List;
 
 /**
  * 用户 服务层。
@@ -9,5 +16,78 @@ import com.nini.niaicodeking.model.entity.User;
  * @author <a herf="https://github.com/XsuperTitan">Blume Ni</a>
  */
 public interface UserService extends IService<User> {
+    /**
+     * 用户注册
+     *
+     * @param userAccount   用户账户
+     * @param userPassword  用户密码
+     * @param checkPassword 校验密码
+     * @return 新用户 id
+     */
+    long userRegister(String userAccount, String userPassword, String checkPassword);
+
+    /**
+     * 加密
+     *
+     * @param userPassword 用户密码
+     * @return 加密后的用户密码
+     */
+    String getEncryptPassword(String userPassword);
+
+    /**
+     * 用户登录
+     *
+     * @param userAccount  用户账户
+     * @param userPassword 用户密码
+     * @param request
+     * @return 脱敏后的用户信息
+     */
+    LoginUserVO userLogin(String userAccount, String userPassword, HttpServletRequest request);
+
+    /**
+     * 获取脱敏的已登录用户信息
+     *
+     * @return
+     */
+    LoginUserVO getLoginUserVO(User user);
+
+    /**
+     * 获取当前登录用户
+     *
+     * @param request
+     * @return
+     */
+    User getLoginUser(HttpServletRequest request);
+
+    /**
+     * 用户注销
+     *
+     * @param request
+     * @return
+     */
+    boolean userLogout(HttpServletRequest request);
+
+    /**
+     * 根据用户实体对象获取用户视图对象
+     *
+     * @param user 用户实体对象，包含用户的基本信息
+     * @return UserVO 用户视图对象，用于前端展示的用户信息
+     */
+    UserVO getUserVO(User user);
+
+
+    /**
+     * 根据用户列表获取用户视图对象列表
+     * @param userList
+     * @return
+     */
+    List<UserVO> getUserVOList(List<User> userList);
+
+    /**
+     * 根据用户查询请求条件构建查询包装器
+     * @param userQueryRequest
+     * @return
+     */
+    QueryWrapper getQueryWrapper(UserQueryRequest userQueryRequest);
 
 }
